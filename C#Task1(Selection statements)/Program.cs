b111bbba8194d55251b6f76fd741599586b5124c
86tokar <86tokar@gmail.com>
2025-07-22 23:29:10 +0200

    Completed task 1.1

--- C#Task1(Selection statements)/Program.cs
+++ C#Task1(Selection statements)/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+void verifyNumber(int num)
+{
+    if (num < 0)
+    {
+        Console.WriteLine("Negative");
+    }
+    else if (num == 0)
+    {
+        Console.WriteLine("Zero");
+    }
+    else
+    {
+        Console.WriteLine("Positive");
+    }
+}
+verifyNumber(3);
