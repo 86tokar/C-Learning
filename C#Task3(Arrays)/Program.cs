894d44941b06b9c871420bf42187a2f9d57d1796
86tokar <86tokar@gmail.com>
2025-10-12 14:52:08 +0200

    add Task #1 for Arrays

---
+++ C#Task3(Arrays)/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        int[] positiveNumbers = GetPositiveArrayFromKeyboard();
+        Task1.VerifyAscArray(positiveNumbers);
+
+
+
+
+        static int GetIntInput(string prompt)
+        {
+            int result;
+            while (true)
+            {
+                Console.Write(prompt);
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out result))
+                {
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input, please inter integer number");
+                }
+            }
+        }
+
+        static int GetPositiveIntInput(string prompt)
+        {
+            int result;
+            while (true)
+            {
+                result = GetIntInput(prompt);
+                if (result > 0)
+                {
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input, enter positive integer number please");
+                }
+            }
+        }
+
+        static int[] GetPositiveArrayFromKeyboard()
+        {
+            Console.Write("Enter amount of elements in the array: ");
+            int amount = GetPositiveIntInput("");
+
+            int[] array = new int[amount];
+
+            for (int i = 0; i < amount; i++)
+            {
+                array[i] = GetPositiveIntInput($"Enter element {i + 1}: ");
+            }
+
+            return array;
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+
+
+
+    }
+}
+
+class Task1
+{
+    public static void VerifyAscArray(int[] positiveNumbers)
+    {
+        for (int i = 0; i < positiveNumbers.Length - 1; i++)
+        {
+            if (positiveNumbers[i] >= positiveNumbers[i + 1])
+            {
+                Console.WriteLine("No");
+                return;
+            }
+        }
+
+        Console.WriteLine("Yes");
+    }
+}
+
+
+
