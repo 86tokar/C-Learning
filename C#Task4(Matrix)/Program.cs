16d65639a8f14ed0da0536ff9e9eb94372b0e9c4
Alexey Tokar <86tokar@gmail.com>
2025-11-09 18:55:41 +0100

    add basic structure and basic method

--- C#Task4(Matrix)/Program.cs
+++ C#Task4(Matrix)/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using C_Task4_Matrix_;
+using System;
+int[,] myMatrix = Methods.CreatePositiveIntMatrix(2, 3);
+
+Console.WriteLine("Your matrix");
+PrintMatrix(myMatrix);
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.ReadKey();
