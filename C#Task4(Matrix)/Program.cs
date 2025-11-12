2c67f5d19e5b28f0ff0d6c48dd63580879a547f6
86tokar <86tokar@gmail.com>
2025-11-12 23:48:22 +0100

    add TaskExecuter

--- C#Task4(Matrix)/Program.cs
+++ C#Task4(Matrix)/Program.cs
@@ -1,13 +1,15 @@
 ﻿using C_Task4_Matrix_;
 
 ITask task1 = new Task1();
-task1.Execute();
-
 ITask task2 = new Task2();
-task2.Execute();
-
 ITask task3 = new Task3();
-task3.Execute();
 
+TaskExecutor executor1 = new TaskExecutor(task1);
+TaskExecutor executor2 = new TaskExecutor(task2);
+TaskExecutor executor3 = new TaskExecutor(task3);
+
+executor1.ExecuteTask();
+executor2.ExecuteTask();
+executor3.ExecuteTask();
 
 Console.ReadKey();
---
+++ C#Task4(Matrix)/TaskExecuter.cs
@@ -0,0 +1,15 @@
+﻿namespace C_Task4_Matrix_
+{
+    public class TaskExecutor
+    {
+        private readonly ITask _task; 
+        public TaskExecutor(ITask task)
+        {
+            _task = task;
+        }
+        public void ExecuteTask()
+        {
+            _task.Execute();
+        }
+    }
+}
