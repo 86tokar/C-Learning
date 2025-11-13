285e5f117fd9d9cb37c0c4976980869809d0f9d0
86tokar <86tokar@gmail.com>
2025-11-13 23:29:15 +0100

    add Task5

--- C#Task4(Matrix)/Program.cs
+++ C#Task4(Matrix)/Program.cs
@@ -1,21 +1,21 @@
 ﻿using C_Task4_Matrix_;
 
-//ITask task1 = new Task1();
-//ITask task2 = new Task2();
-//ITask task3 = new Task3();
-//ITask task4 = new Task4();
+ITask task1 = new Task1();
+ITask task2 = new Task2();
+ITask task3 = new Task3();
+ITask task4 = new Task4();
 ITask task5 = new Task5();
 
-//TaskExecutor executor1 = new TaskExecutor(task1);
-//TaskExecutor executor2 = new TaskExecutor(task2);
-//TaskExecutor executor3 = new TaskExecutor(task3);
-//TaskExecutor executor4 = new TaskExecutor(task4);
+TaskExecutor executor1 = new TaskExecutor(task1);
+TaskExecutor executor2 = new TaskExecutor(task2);
+TaskExecutor executor3 = new TaskExecutor(task3);
+TaskExecutor executor4 = new TaskExecutor(task4);
 TaskExecutor executor5 = new TaskExecutor(task5);
 
-//executor1.ExecuteTask();
-//executor2.ExecuteTask();
-//executor3.ExecuteTask();
-//executor4.ExecuteTask();
+executor1.ExecuteTask();
+executor2.ExecuteTask();
+executor3.ExecuteTask();
+executor4.ExecuteTask();
 executor5.ExecuteTask();
 
 Console.ReadKey();
